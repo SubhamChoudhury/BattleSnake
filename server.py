2f2f5561e38efd73f4591ef2fb4b06c974448348
Subham Choudhury <55877612+SubhamChoudhury@users.noreply.github.com>
2021-06-27 22:45:12 +0530

    Modifications from Day-3

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 import random
-
+import snakebrain
 import cherrypy
 
 """
@@ -10,9 +10,15 @@ For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python
 
 
 class Battlesnake(object):
+    game_id = ""
+    turn = -1
+
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
+        # This function is called when you register your Battlesnake on play.battlesnake.com
+        # It controls your Battlesnake appearance and author permissions.
+        # TIP: If you open your Battlesnake URL in browser you should see this data
         return {
             "apiversion": "1",
             "author": "Subham_Choudhury",  
@@ -26,79 +32,46 @@ class Battlesnake(object):
     def start(self):
         # This function is called everytime your snake is entered into a game.
         # cherrypy.request.json contains information about the game that's about to be played.
+        # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
 
-        
+        self.turn = data["turn"]
+        self.game_id = data["game"]["id"]
+
         return "ok"
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def move(self):
+        # This function is called on every turn of a game. It's how your snake decides where to move.
+        # Valid moves are "up", "down", "left", or "right".
         data = cherrypy.request.json
-        body= data["you"]["body"]
+
+        self.turn = data["turn"]
+        self.game_id = data["game"]["id"]
+        body = data["you"]["body"]
+
+        # Choose a direction to move in
         possible_moves = ["up", "down", "left", "right"]
-        safe_moves=self.getSafeMoves(possible_moves,body,data["board"])
 
-        if safe_moves:
-          move=random.choice(safe_moves)
-          return {"move": move}
-        return{"move":'down'}
-    def getNext(self,currentHead, nextMove):
-       futureHead = currentHead.copy()
-       if nextMove == 'left':
-         futureHead['x']=currentHead['x']- 1
-       if nextMove == 'right':
-         futureHead['x']=currentHead['x']+ 1
-       if nextMove == 'up':
-         futureHead['y']=currentHead['y']+ 1
-       if nextMove == 'down':
-         futureHead['y']=currentHead['y']- 1
-       return futureHead
+        smart_moves = snakebrain.get_smart_moves(possible_moves, body, data["board"])
 
-    def getSafeMoves(self,possible_moves,body,board):
-       safe_moves = []
-       for guess in possible_moves:
-         guessCoord = self.getNext(body[0],guess)
-         if self.avoidWalls(guessCoord,board["width"],board["height"]) and self.avoidSnakes(guessCoord, board["snakes"]):
-           safe_moves.append(guess)
-         elif len(body) > 1 and guessCoord == body[-1] and guess not in body[:-1]:
-           safe_moves.append(guess)
+        move = random.choice(smart_moves)
 
-       return safe_moves
-
-    def avoidWalls(self,futureHead,width,height):
-      result = True
-
-      x = int(futureHead['x'])
-      y = int(futureHead['y'])
-      
-      if x<0 or y<0 or x>= width or y>=height:
-        result =  False
-      return result
-
-    def avoidSnakes(self, futureHead, snakeBodies):
-      for snake in snakeBodies:
-        if futureHead in snake["body"][:-1]:
-          return False
-        return True
+        return {"move": move}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def end(self):
-        # This function is called when a game your snake was in ends.
-        # It's purely for informational purposes, you don't have to make any decisions here.
-        data = cherrypy.request.json
-
-        print("END")
         return "ok"
-
 
 if __name__ == "__main__":
     server = Battlesnake()
+    cherrypy.config.update({"server.thread.pool": 10})
     cherrypy.config.update({"server.socket_host": "0.0.0.0"})
     cherrypy.config.update(
-        {"server.socket_port": int(os.environ.get("PORT", "8080")),}
+        {"server.socket_port": int(os.environ.get("PORT", "8080")), }
     )
     print("Starting Battlesnake Server...")
     cherrypy.quickstart(server)
